7a4c07787188dfde77f1fd6d11f40b05fdf819e8
Henrique Melo <henrique@pampadevs.com>
2021-01-12 13:45:04 -0300

    Imagemin Plugin

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,32 @@
+const Imagemin = require('imagemin-webpack-plugin').default;
+
+/*
+new Imagemin({
+    pngquant: '95-100'
+})
+const ImageminWebp = require('imagemin-webp-webpack-plugin');
+new ImageminWebp({
+    config: [
+        {
+            test: /\.(jpe?g|png)/,
+            options: {
+                quality: 50
+            }
+        }
+    ],
+    overrideExtension: true,
+    strict: true
+})
+*/
+
 module.exports = {
+    configureWebpack: {
+        plugins: [
+            new Imagemin({
+                pngquant: '95-100'
+            })
+        ]
+    },
     css: {
         loaderOptions: {
             sass: {
